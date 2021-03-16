06addd44003ece4c94677dd904faf31881b52979
Pusha <de24world@gmail.com>
2021-03-16 23:21:33 +0100

    05 폼에서 useState 사용하기

---
+++ src/05.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+function App05() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(username, password);
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App05;
