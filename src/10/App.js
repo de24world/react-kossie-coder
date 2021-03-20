7f75784c69b9a44c9149b99cbdbca83a414a2a7f
Pusha <de24world@gmail.com>
2021-03-20 21:47:44 +0100

    10 리액트 반복문

---
+++ src/10/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./index.css";
+
+function App() {
+  const movies = [
+    { title: "kossie coder1", year: 2001 },
+    { title: "kossie coder2", year: 2002 },
+    { title: "kossie coder3", year: 2003 },
+    { title: "kossie coder4", year: 2004 }
+  ];
+  const renderMovies = movies.map((movie) => {
+    return (
+      <div className="movie" key={movie.title}>
+        <div className="movie-title">{movie.title}</div>
+        <div className="movie-year">{movie.year}</div>
+      </div>
+    );
+  });
+  return (
+    <div className="App">
+      <h1>Movie list</h1>
+      {renderMovies}
+    </div>
+  );
+}
+
+export default App;
