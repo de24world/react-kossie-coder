597fe8df32821dfad9fd9628d1174dbac24c698e
Pusha <de24world@gmail.com>
2021-03-31 22:47:10 +0200

    22 NavLink 사용하기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
-import App from "./20/App";
+import App from "./22/App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
